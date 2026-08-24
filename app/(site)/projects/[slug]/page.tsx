f860d9fd6e33287221f5bfbc5b070e5080030e7e
محمد زين حميدي <223725386+ZainHDY@users.noreply.github.com>
2026-08-24 11:44:06 +0300

    Fix indentation in return statement

--- app/(site)/projects/[slug]/page.tsx
+++ app/(site)/projects/[slug]/page.tsx
@@ -58,7 +58,7 @@ export default async function ProjectPage({ params }: { params: { slug: string }
     keywords: project.tags?.join(', '),
   };
 
-  return (
+   return (
     <section>
       <script
         type="application/ld+json"
