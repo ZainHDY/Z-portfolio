9a75f42bb3e4ba41186054346918ba50da3c5538
محمد زين حميدي <223725386+ZainHDY@users.noreply.github.com>
2026-09-06 18:04:43 +0300

    Register error page Sanity schema

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -2,7 +2,8 @@ import siteSettings from './siteSettings';
 import category from './category';
 import project from './project';
 import experience from './experience';
+import errorPage from './errorPage';
 import localizedText from './localizedText';
 import localizedPortableText from './localizedPortableText';
 
-export const schemaTypes = [siteSettings, category, project, experience, localizedText, localizedPortableText];
+export const schemaTypes = [siteSettings, category, project, experience, errorPage, localizedText, localizedPortableText];
